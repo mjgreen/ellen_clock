dd9e2fd22f905d3e94b066ef1f3d19316cf682c8
mgreen <mgreen1718@gmail.com>
2020-01-14 06:06:53 +0000

    changes for python3 compliance

--- main.py
+++ main.py
@@ -9,26 +9,27 @@ mon.setSizePix((1920, 1080))
 
 # open the window
 win = visual.Window(fullscr=False,  size=[900, 900], screen=0, color=[0, 0, 0], units="pix", allowGUI=True,
-                    waitBlanking=True, monitor="e330", autoLog=False, winType='pyglet', gammaErrorPolicy = "ignore")
+                    waitBlanking=True, monitor="e330", autoLog=False, winType='pyglet', gammaErrorPolicy="ignore")
 
 # specify the coordinates of the numbers on the clock perimeter
 thetas = np.linspace(start=360+90, stop=90, num=12, endpoint=False, retstep=False, dtype='f8')
 xs, ys = psychopy.tools.coordinatetools.pol2cart(thetas, radius=300+20)
-coords = zip(xs, ys)
+xs = xs + 1920/4.0
+coords = list(zip(xs, ys))
 
 # give the coordinates of each increment's position on the clock perimeter, for use in rotating the hand smoothly
 thetas = np.linspace(start=360+90, stop=90, num=60*4, endpoint=False, retstep=False, dtype='f8')
 xs, ys = psychopy.tools.coordinatetools.pol2cart(thetas, radius=300)
-second_positions = zip(xs, ys)
+second_positions = list(zip(xs, ys))
 
 # Make a stimulus for the clock's perimeter
-clock_circle = visual.Circle(win, radius = 300, edges=32*8, pos=(0,0), name='clock')
+clock_circle = visual.Circle(win, radius=300, edges=32*8, pos=(0, 0), name='clock')
 
 # Make a stimulus for the clock's centre
-clock_centre = visual.Circle(win, radius = 5, edges=32*8, pos=(0,0), name='center', fillColor='white')
+clock_centre = visual.Circle(win, radius=5, edges=32*8, pos=(0, 0), name='center', fillColor='white')
 
 # specify the text for the numbers on the clock face
-clock_text = ['60'] + [str(x) for x in range(5, 60,5)]
+clock_text = ['60'] + [str(x) for x in range(5, 60, 5)]
 
 # draw the clock into a movie frame
 # TODO add tick marks
@@ -45,9 +46,9 @@ clock_stimulus = visual.ImageStim(win, image="clock.jpg")
 
 # Draw the clock, and make the hand sweep round a few times
 
-for number_of_revolutions in range(1):
+for number_of_revolutions in range(2):
     for secs in range(60*4):
-        myline = visual.Line(win, start=(0,0), end=second_positions[secs], lineWidth=8)
+        myline = visual.Line(win, start=(0, 0), end=second_positions[secs], lineWidth=8)
         clock_stimulus.draw()
         myline.draw()
         win.flip()
